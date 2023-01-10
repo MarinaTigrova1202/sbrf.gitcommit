79ff9c238ebf36329efa07da6734aaba484a059f
MarinaTigrova1204 <118852910+MarinaTigrova1204@users.noreply.github.com>
2023-01-10 12:16:19 +0300

    Добавила новое задание

--- src/com/max/idea/Main.java
+++ src/com/max/idea/Main.java
@@ -6,6 +6,5 @@ public class Main {
     public static void main(String[] args) {
         Car car = new Car();
         System.out.println(car.toString());
-//        System.out.println(print);
     }
 }
