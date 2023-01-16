59af431acbedcad2f881a31522cb9ff58fe219d4
MarinaTigrova1204 <118852910+MarinaTigrova1204@users.noreply.github.com>
2023-01-17 00:38:49 +0300

    Исправила 2 задание и добавила 3

--- src/ru/sbrf/gitcommit/week4/Main.java
+++ src/ru/sbrf/gitcommit/week4/Main.java
@@ -1,72 +1,118 @@
 package ru.sbrf.gitcommit.week4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         int value = 0;
         int number = 0;
-        //цикл while
+
+        System.out.println("цикл while");
         while (value < 11) {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
             value = value + 2;
         }
+
         value = 9;
+        number = 0;
         while (value >= 0) {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
-            value = value - 2;
+            value -= 2;
         }
-        //цикл for
+
+        System.out.println();
+        System.out.println("цикл for");
+        number = 0;
         for (value = 0; value <= 10; value = value + 2) {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
         }
+
+        number = 0;
         for (value = 9; value >= 0; value = value - 2) {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
         }
-        //цикл do while
+
+        System.out.println();
+        System.out.println("цикл do while");
         value = 0;
+        number = 0;
         do {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
             value = value + 2;
         } while (value <= 10);
+
         value = 9;
+        number = 0;
         do {
             number++;
-            if (number == 3) {
+            if (number % 3 == 0) {
                 System.out.println(value + "!");
-                number = number - 3;
             } else {
                 System.out.println(value);
             }
-            value = value - 2;
+            value -= 2;
         } while (value >= 0);
+
+        System.out.println();
+        System.out.println("Оператор switch");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите возраст");
+        int age = scanner.nextInt();
+        switch (age) {
+            case 0:
+                System.out.println("Ты родился");
+                break;
+            case 7:
+                System.out.println("Ты пошел в школу");
+                break;
+            case 10:
+                System.out.println("Ты закончил младшую школу");
+                break;
+            case 11:
+                System.out.println("Ты пошел в старшую школу");
+                break;
+            case 17:
+                System.out.println("Ты закончил школу");
+                break;
+            case 18:
+                System.out.println("Ты пошел в университет");
+                break;
+            case 22:
+                System.out.println("Ты пошел работать");
+                break;
+            case 65:
+                System.out.println("Добро подаловать на пенсию");
+                break;
+            case 100:
+                System.out.println("Game over");
+                break;
+            default:
+                System.out.println("ни одно из предыдущих условий не подошло");
+        }
     }
 }
